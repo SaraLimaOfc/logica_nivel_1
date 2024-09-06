adff5e12af71987988fb46f6ad23083b8b20c72f
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-09-06 19:21:58 -0300

    Exibir menu na tela

---
+++ exec-6.py
@@ -0,0 +1,9 @@
+def exibir_menu():
+    print("Cadastro de Clientes:")
+    print("0 - Fim")
+    print("1 - Inclui")
+    print("2 - Altera")
+    print("3 - Exclui")
+    print("4 - Consulta")
+
+exibir_menu()
